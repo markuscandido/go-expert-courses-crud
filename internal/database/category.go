98d0ee2cfa9db6dd1e61a9555a996b0ec2601d84
Markus Vinicius Candido <markus.candido@gmail.com>
2025-08-13 18:32:48 -0300

    feat: add CourseDB

--- internal/database/category.go
+++ internal/database/category.go
@@ -17,13 +17,13 @@ func NewCategory(db *sql.DB) *Category {
 	return &Category{db: db}
 }
 
-func (c *Category) Create(name, description string) (Category, error) {
+func (c *Category) Create(name, description string) (*Category, error) {
 	id := uuid.New().String()
 	_, err := c.db.Exec("INSERT INTO categories (id, name, description) VALUES ($1, $2, $3)", id, name, description)
 	if err != nil {
-		return Category{}, err
+		return nil, err
 	}
-	return Category{
+	return &Category{
 		ID:          id,
 		Name:        name,
 		Description: description,
